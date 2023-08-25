dff0d4aa6cad187cac4015b244a178f2f2ab4589
yasser_abbasi <yasser_abbasi@yahoo.com>
2023-08-25 17:34:22 +0100

    fix(infrastructure): Update output variable names

--- infrastructure/code/modules/cloudfront/outputs.tf
+++ infrastructure/code/modules/cloudfront/outputs.tf
@@ -1,11 +1,15 @@
-output "cloudfront_arn" {
+output "cloudfront_distribution_arn" {
   value = aws_cloudfront_distribution.s3_distribution.arn
 }
 
-output "cloudfront_domain_name" {
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.s3_distribution.id
+}
+
+output "cloudfront_distribution_domain_name" {
   value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
 
-output "cloudfront_hosted_zone_id" {
+output "cloudfront_distribution_hosted_zone_id" {
   value = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
 }
