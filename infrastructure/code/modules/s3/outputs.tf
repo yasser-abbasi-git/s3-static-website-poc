bd57da61ad7d325dd569bff206f2d85986c93e2f
yasser_abbasi <yasser_abbasi@yahoo.com>
2023-08-25 17:35:18 +0100

    feat(infrastructure): Add s3 module output variables

--- infrastructure/code/modules/s3/outputs.tf
+++ infrastructure/code/modules/s3/outputs.tf
@@ -1,15 +1,11 @@
-# output "bucket_website_domain" {
-#   value = aws_s3_bucket_website_configuration.website.website_domain
-# }
-
-# output "bucket_hosted_zone_id" {
-#   value = aws_s3_bucket.website.hosted_zone_id
-# }
-
-# output "bucket_name" {
-#   value = aws_s3_bucket.website.bucket
-# }
-
 output "bucket_regional_domain_name" {
   value = aws_s3_bucket.website.bucket_regional_domain_name
+}
+
+output "bucket_id" {
+  value = aws_s3_bucket.website.id
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.website.arn
 }
