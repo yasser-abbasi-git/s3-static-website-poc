d2fc644eb11f7e33565f3d49ae9ca7e1c624b173
yasser_abbasi <yasser_abbasi@yahoo.com>
2023-08-28 14:27:39 +0100

    fix(infrastructure): Fix formatting

--- infrastructure/code/modules/s3/main.tf
+++ infrastructure/code/modules/s3/main.tf
@@ -1,6 +1,6 @@
 # Create bucket for the static website
 resource "aws_s3_bucket" "website" {
-  bucket = var.bucket_name
+  bucket        = var.bucket_name
   force_destroy = true
   tags = {
     Name = "${var.project_name}-bucket"
